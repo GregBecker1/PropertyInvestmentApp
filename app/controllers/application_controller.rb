dbdccc6609e17ff16960b097ec2b98833f0985e2
Greg Becker <glbecker@madisoncollege.edu>
2015-03-12 20:42:07 +0000

    commit for route config and new def in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def greeting
+    render text: "Welcome to the Real Estate Investment Property Comparison Application.......Under Construction, please come back shortly."
+  end
 end
