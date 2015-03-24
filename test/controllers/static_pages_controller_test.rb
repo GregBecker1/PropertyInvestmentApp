b5a582f2d210fa6086cd515f78c8a104dd2ffc99
Greg Becker <glbecker@madisoncollege.edu>
2015-03-24 21:05:16 +0000

    Used DRY method to clean up pages changed routes and make a test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Investment Property Comparison App"    
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | Investment Property Comparison App"    
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Investment Property Comparison App"    
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Investment Property Comparison App"
+  end  
 
 end
