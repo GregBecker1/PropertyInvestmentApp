d57f1f7c8ed952df3f425328a9cdd5fbf8372cd0
Greg Becker <glbecker@madisoncollege.edu>
2015-03-26 20:09:31 +0000

    commit for adding css and signup page

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,10 @@ Rails.application.routes.draw do
   resources :users
   
   root 'static_pages#home'
-  
-  get 'static_pages/contact'
-  
-  get 'static_pages/home'
+  get 'help' => 'static_pages#help'  
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
 
-  get 'static_pages/about'
-
-  get 'static_pages/help'
 
 end
