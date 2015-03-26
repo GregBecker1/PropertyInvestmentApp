a31b9ec8b87d2a5de17f2557ec1d6203fec87a56
Greg Becker <glbecker@madisoncollege.edu>
2015-03-26 21:51:12 +0000

    validate attributes, presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :investment_properties
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(firstname: "firstname", lastname: "lastname",  email: "user@example.com", phone: "608-555-1212")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "firstname should be present" do
+    @user.firstname = "    "
+    assert_not @user.valid?
+  end
+  
+  test "lastname should be present" do
+    @user.lastname = "    "
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+  
+  test "phone should be present" do
+    @user.phone = "    "
+    assert_not @user.valid?
+  end
 end
